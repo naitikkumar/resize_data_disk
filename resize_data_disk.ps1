48ed800b633cf787919bbe126ab9759676800047
naitikkumar <r.naitikkumar1992@gmail.com>
2021-03-27 18:42:21 +0530

    Update resize_data_disk.ps1

--- resize_data_disk.ps1
+++ resize_data_disk.ps1
@@ -5,7 +5,7 @@ param(
 [int]$disknumber
 )
 $vm = Get-AzVM -ResourceGroupName $rgName -Name $vmName
-Stop-AzVM -ResourceGroupName $rgName -Name $vmName -Confirm $false
+Stop-AzVM -ResourceGroupName $rgName -Name $vmName -Confirm $false -force
 Get-AzDisk -ResourceGroupName $rgName -DiskName $vm.StorageProfile.DataDisks[$disknumber].Name
 $disk.DiskSizeGB = $size
 Update-AzDisk -ResourceGroupName $rgName -Disk $disk -DiskName $disk.Name
